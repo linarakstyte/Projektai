e9461c8fb53c98670a006a03c78fb477ca0eb095
linarakstyte <94565077+linarakstyte@users.noreply.github.com>
2021-12-08 15:03:18 +0200

    Ikelimas

---
+++ 7Paskaita/7Paskaita/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7Paskaita
+{
+    class Program
+    {
+
+        //public struct Mokinys
+        //{
+        //    public List<int> PirmasSemestras;
+        //    public List<int> AntrasSemestras;
+        //    public List<int> TreciasSemestras;
+
+
+        //    public Mokinys(List<int> pirmasSemetras, List<int> antrasSemetras, List<int> treciasSemetras)
+        //    {
+        //        PirmasSemestras = pirmasSemetras;
+        //        AntrasSemestras = antrasSemetras;
+        //        TreciasSemestras = treciasSemetras;
+        //    }
+
+        //    public void MetinisVidurkis()
+        //    {
+        //        double vidurkis1 = PirmasSemestras.Average();
+        //        double vidurkis2 = AntrasSemestras.Average();
+        //        double vidurkis3 = TreciasSemestras.Average();
+        //        Console.WriteLine("Metinis vidurkis: ", (vidurkis1 + vidurkis2 + vidurkis3) / 3);
+
+        //    }
+
+
+
+
+        //    static void Main(string[] args)
+        //    {
+        //        Mokinys mokinys = new Mokinys { (5, 7, 8, 5),(8, 6, 8, 9),(5, 6, 3, 6) };
+
+
+        //    }
+        //}
+
+       
+
+        //    public struct BibliotekosKnyga
+        //    {
+        //        int ID;
+        //        string Pavadinimas;
+        //        string Vardas;
+        //        public DateTime Data;
+
+        //        public BibliotekosKnyga(int id, string pavadinimas, string vardas, DateTime data)
+        //        {
+        //            ID = id;
+        //            Pavadinimas = pavadinimas; ;
+        //            Vardas = vardas; ;
+        //            Data = data;
+        //        }
+            
+        //        public KiekDienu (DateTime data)
+        //        {
+        //            DateTime
+
+        //        }
+
+        //        static void Main(string[] args)
+        //    {
+        //        Mokinys mokinys = new Mokinys("Petras", "Jonaitis", "Spalis", "Liepa");
+        //        Console.WriteLine($"{ mokinys.Vardas} {mokinys.Pavarde} {mokinys.KursoPradzia} {mokinys.KursoPabaiga}");
+
+
+           // }
+
+
+
+        }
+    }
+}
