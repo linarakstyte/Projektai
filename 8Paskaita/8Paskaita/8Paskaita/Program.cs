5ccf0a1abd3bb8fae24ab085e6a5ccc18a9d300b
linarakstyte <94565077+linarakstyte@users.noreply.github.com>
2021-12-13 11:47:43 +0200

    8 Paskaita prideta

---
+++ 8Paskaita/8Paskaita/8Paskaita/Program.cs
@@ -0,0 +1,18 @@
+﻿using BookStruct;
+using System;
+
+namespace _8Paskaita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Super Knyga", "124");
+
+            var book1 = new Book("Super Knyga", "124");
+
+            book.TestProperty = 5;
+            var value = book.TestProperty;
+        }
+    }
+}
---
+++ 8Paskaita/8Paskaita/BookStruct/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BookStruct
+{
+    public struct Book
+
+    {
+        public int TestProperty { get; private set; };
+        private string _name;
+        private string _id;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set 
+            {
+                _name = value;
+                CreateLocalId();
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set 
+            {
+                _id = value;
+                CreateLocalId();
+            }
+        }
+        public string LocalId { get; private set; }
+
+        public Book (string name, string id)
+        {
+            _name = name;
+            _id = id;
+            LocalId = string.Empty; 
+            CreateLocalId();
+
+        }
+        private void CreateLocalId()
+        {
+            LocalId = string.Format("{0}_{1}", Name, Id);
+        }
+    }
+}
