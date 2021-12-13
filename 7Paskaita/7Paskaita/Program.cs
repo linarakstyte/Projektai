e904909ab2d7cf451a00311575c1e200ef574a23
linarakstyte <94565077+linarakstyte@users.noreply.github.com>
2021-12-13 10:23:15 +0200

    Savarankiskas darbas

--- 7Paskaita/7Paskaita/Program.cs
+++ 7Paskaita/7Paskaita/Program.cs
@@ -7,91 +7,111 @@ namespace _7Paskaita
     class Program
     {
 
-        //public struct Mokinys
-        //{
-        //    public List<int> PirmasSemestras;
-        //    public List<int> AntrasSemestras;
-        //    public List<int> TreciasSemestras;
-
-
-        //    public Mokinys(List<int> pirmasSemetras, List<int> antrasSemetras, List<int> treciasSemetras)
-        //    {
-        //        PirmasSemestras = pirmasSemetras;
-        //        AntrasSemestras = antrasSemetras;
-        //        TreciasSemestras = treciasSemetras;
-        //    }
-
-        //    public void MetinisVidurkis()
-        //    {
-        //        double vidurkis1 = PirmasSemestras.Average();
-        //        double vidurkis2 = AntrasSemestras.Average();
-        //        double vidurkis3 = TreciasSemestras.Average();
-        //        Console.WriteLine("Metinis vidurkis: ", (vidurkis1 + vidurkis2 + vidurkis3) / 3);
-
-        //    }
-
-
-
-
-        //    static void Main(string[] args)
-        //    {
-        //        Mokinys mokinys = new Mokinys { (5, 7, 8, 5),(8, 6, 8, 9),(5, 6, 3, 6) };
-
-
-        //    }
-        //}
-
-
-
-        //    public struct BibliotekosKnyga
-        //    {
-        //        int ID;
-        //        string Pavadinimas;
-        //        string Vardas;
-        //        public DateTime Data;
-
-        //        public BibliotekosKnyga(int id, string pavadinimas, string vardas, DateTime data)
-        //        {
-        //            ID = id;
-        //            Pavadinimas = pavadinimas; ;
-        //            Vardas = vardas; ;
-        //            Data = data;
-        //        }
-
-        //        public KiekDienu (DateTime data)
-        //        {
-        //            DateTime
-
-        //        }
-
-        //        static void Main(string[] args)
-        //    {
-        //        Mokinys mokinys = new Mokinys("Petras", "Jonaitis", "Spalis", "Liepa");
-        //        Console.WriteLine($"{ mokinys.Vardas} {mokinys.Pavarde} {mokinys.KursoPradzia} {mokinys.KursoPabaiga}");
-
-
-        // }
-
-        public static decimal SkaiciuotiMokescius(decimal pajamos)
+        public struct Mokinys
         {
-            decimal rezultatas = 0;
+            public int [] PirmasSemestras;
+            public int [] AntrasSemestras;
+            public int [] TreciasSemestras;
 
-            if (pajamos <= 400)
+
+            public Mokinys(int [] pirmasSemetras, int[] antrasSemetras, int[] treciasSemetras)
             {
-                rezultatas = pajamos * 0.05m;
-            }
-            else if (pajamos > 400 && pajamos <= 1000)
-            {
-                rezultatas = pajamos * 0.15m;
-            }
-            else
-            {
-                rezultatas = pajamos * 0.25m;
+                PirmasSemestras = pirmasSemetras;
+                AntrasSemestras = antrasSemetras;
+                TreciasSemestras = treciasSemetras;
             }
 
-            return rezultatas;
+            public void MetinisVidurkis()
+            {
+                double vidurkis1 = PirmasSemestras.Average();
+                double vidurkis2 = AntrasSemestras.Average();
+                double vidurkis3 = TreciasSemestras.Average();
+                Console.WriteLine($"Semestru vidurkiai: {vidurkis1}, {vidurkis2}, {vidurkis3}");
+                Console.WriteLine($"Metinis vidurkis: {(vidurkis1 + vidurkis2 + vidurkis3)/3}");
+
+            }
+
+            public struct BibliotekosKnyga
+            {
+                int ID;
+                string Pavadinimas;
+                string Vardas;
+                public DateTime Data;
+
+                public BibliotekosKnyga(int id, string pavadinimas, string vardas, DateTime data)
+                {
+                    ID = id;
+                    Pavadinimas = pavadinimas; ;
+                    Vardas = vardas; ;
+                    Data = data;
+                }
+
+                public int KiekDienu(DateTime data)
+                {
+                    DateTime today = DateTime.Today;
+                    int dienuSkaicius = (today - data).Days;
+
+                    return dienuSkaicius;
+
+                }
+                public struct Staciakampis
+                {
+                    int Ilgis;
+                    int Plotis;
+
+                    public int Plotas(int ilgis, int plotis)
+                    {
+                        Ilgis = ilgis;
+                        Plotis = plotis;
+                        return (ilgis * plotis);
+                    }
+
+
+                    static void Main(string[] args)
+                    {
+                        //Mokinys mokinys = new Mokinys("Petras", "Jonaitis", "Spalis", "Liepa");
+                        //Console.WriteLine($"{ mokinys.Vardas} {mokinys.Pavarde} {mokinys.KursoPradzia} {mokinys.KursoPabaiga}");
+                        BibliotekosKnyga knyga = new BibliotekosKnyga(4841012, "Svetimas", "Lina", new DateTime(2020, 10, 10));
+                        int dienos = knyga.KiekDienu(new DateTime(2020, 10, 10));
+                        Console.WriteLine($"{knyga.Vardas} buvo pasimusi/emes knyga {knyga.Pavadinimas} {dienos} dienas/u");
+
+                        Staciakampis staciakampis1 = new Staciakampis();
+
+                        Staciakampis staciakampis2 = new Staciakampis();
+
+                        Staciakampis staciakampis3 = new Staciakampis();
+
+
+                        Console.WriteLine($"Pasirinktu staciakampiu plotai: {staciakampis1.Plotas(10, 10)}, {staciakampis2.Plotas(20, 20)}, {staciakampis1.Plotas(30, 30)}");
+
+                        Mokinys mokinys = new Mokinys(new int[] {0, 3, 5, 9, 9}, new int[] {4, 3, 4, 7, 9},new  int[] {1, 3, 5, 7, 9});
+                        mokinys.MetinisVidurkis();
+                    }
+                }
+                #region
+                //public static decimal SkaiciuotiMokescius(decimal pajamos)
+                //{
+                //    decimal rezultatas = 0;
+
+                //    if (pajamos <= 400)
+                //    {
+                //        rezultatas = pajamos * 0.05m;
+                //    }
+                //    else if (pajamos > 400 && pajamos <= 1000)
+                //    {
+                //        rezultatas = pajamos * 0.15m;
+                //    }
+                //    else
+                //    {
+                //        rezultatas = pajamos * 0.25m;
+                //    }
+
+                //    return rezultatas;
+                #endregion
+            }
+
         }
+    }
+    }
 
-    }
-    }
-}
+
---
+++ 7Paskaita/Savarankiski/Savarankiski/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace Savarankiski
+{
+    class Program
+    {
+        public struct Studentas
+        {
+            public int[] PirmasSemestras;
+            public int[] AntrasSemestras;
+            public int[] TreciasSemestras;
+
+
+            public Studentas(int[] pirmasSemetras, int[] antrasSemetras, int[] treciasSemetras)
+            {
+                PirmasSemestras = pirmasSemetras;
+                AntrasSemestras = antrasSemetras;
+                TreciasSemestras = treciasSemetras;
+            }
+
+            public void MetinisVidurkis()
+            {
+                double vidurkis1 = PirmasSemestras.Average();
+                double vidurkis2 = AntrasSemestras.Average();
+                double vidurkis3 = TreciasSemestras.Average();
+                Console.WriteLine($"Semestru vidurkiai: {vidurkis1}, {vidurkis2}, {vidurkis3}");
+                Console.WriteLine($"Metinis vidurkis: {(vidurkis1 + vidurkis2 + vidurkis3) / 3}");
+
+            }
+
+            public struct Rinkejas
+            {
+                public int ID;
+                public string Balsavo;
+
+                public Rinkejas(int id, string balsavo)
+                {
+                    ID = id;
+                    Balsavo = balsavo;
+                }
+
+                static void Main(string[] args)
+                {
+                    string [] sarasas = new string[10];
+                    int balsavo = 0;
+                    Studentas studentas = new Studentas(new int[] { 10, 3, 5, 9, 9 }, new int[] { 4, 3, 4, 7, 9 }, new int[] { 10, 3, 5, 7, 9 });
+                    studentas.MetinisVidurkis();
+
+                    Rinkejas rinkejas1 = new Rinkejas(193, "balsavo");
+                    sarasas[0] = rinkejas1.Balsavo;
+                    Rinkejas rinkejas2 = new Rinkejas(234, "balsavo");
+                    sarasas[1] = rinkejas2.Balsavo;
+                    Rinkejas rinkejas3 = new Rinkejas(343, "balsavo");
+                    sarasas[2] = rinkejas3.Balsavo;
+                    Rinkejas rinkejas4 = new Rinkejas(673, "nebalsavo");
+                    sarasas[3] = rinkejas4.Balsavo;
+                    Rinkejas rinkejas5 = new Rinkejas(553, "balsavo");
+                    sarasas[4] = rinkejas5.Balsavo;
+                    balsavo = sarasas.Count(s => s == "balsavo");
+                    Console.WriteLine($"Is 5 rinkeju {balsavo} balsavo ir {(5 - balsavo)} nebalsavo.");
+                }
+            }
+        }
+    }
+}
